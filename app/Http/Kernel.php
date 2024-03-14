ff17303bb74cbcdc908e318177daf6939a7a6182
Elvis  Yerel Roman Concha <yerel9212@yahoo.es>
2024-03-13 21:42:26 -0500

    Disable XCSF-TOKEN

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,13 +33,13 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            //\Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            //\App\Http\Middleware\VerifyCsrfToken::class,
+            //\Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':180,1',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':180,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
@@ -63,10 +63,10 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-		'server' => \Elyerr\Passport\Connect\Middleware\Authorization::class,
-		'scope' => \Elyerr\Passport\Connect\Middleware\CheckForAnyScope::class,
-		'scopes' => \Elyerr\Passport\Connect\Middleware\CheckScopes::class,
-		'client' => \Elyerr\Passport\Connect\Middleware\CheckClientCredentials::class,
-		'transform.request' => \Elyerr\ApiResponse\Middleware\TransformRequest::class,
+        'server' => \Elyerr\Passport\Connect\Middleware\Authorization::class,
+        'scope' => \Elyerr\Passport\Connect\Middleware\CheckForAnyScope::class,
+        'scopes' => \Elyerr\Passport\Connect\Middleware\CheckScopes::class,
+        'client' => \Elyerr\Passport\Connect\Middleware\CheckClientCredentials::class,
+        'transform.request' => \Elyerr\ApiResponse\Middleware\TransformRequest::class,
     ];
 }
