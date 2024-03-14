c189e5a966bad2918d0a7f245365d85e6af6ffa2
Elvis  Yerel Roman Concha <yerel9212@yahoo.es>
2024-03-13 21:44:48 -0500

    Updated app.js GUards

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import { $server, $host } from "./config/axios";
 import * as bootstrap from "bootstrap";
 import { $echo, $channels } from "./config/echo";
 
-import App from "./App.vue"; 
+import App from "./App.vue";
 
 /**
  * Cheking Routes from VueRouter
@@ -13,27 +13,28 @@ router.beforeEach((to, from, next) => {
     /**
      * checking for valid credentials
      */
-    if (to.meta.auth) {
-        $server
-            .get("/api/gateway/check-authentication")
-            .then((res) => {
-                /**
-                 * redirect user to the home page
-                 */
+    $server
+        .get("/api/gateway/check-authentication")
+        .then((res) => {
+            /**
+             * Checking if the route is auth
+             */
+            if (to.meta.auth) {
                 next();
-            })
-            .catch((err) => {
+            } else if (!to.meta.auth && to.name == "login") {
                 /**
-                 * Has a not valid crdential redirect to le login
+                 * Ckecking the user is auth and the route is
+                 * login we're redirect to the user to calendar route
                  */
-                return next({ name: "login" });
-            });
-    } else {
-        /**
-         * if the route is no auth route pass next request
-         */
-        next();
-    }
+                return next({ name: "calendar" });
+            }
+        })
+        .catch((err) => {
+            /**
+             * Has a not valid crdential redirect to le login
+             */
+            next();
+        });
 });
 /**
  * creating Vue App
@@ -43,7 +44,6 @@ const app = createApp(App);
 /**
  * Global properties for Vuejs
  */
-//app.config.globalProperties.$id = res.data.id;
 app.config.globalProperties.$server = $server;
 app.config.globalProperties.$host = $host;
 app.config.globalProperties.$echo = $echo;
