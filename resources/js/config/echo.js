1926ae657c67ed4ae4f68cbb0dfb046e90b72b4b
Elvis Yerel Roman C <yerel9212@yahoo.es>
2024-04-18 22:47:19 -0500

    Fixed Authorization header to utilize channels

--- resources/js/config/echo.js
+++ resources/js/config/echo.js
@@ -1,9 +1,13 @@
 import EchoClient from "echo-client-js";
+import Cookies from "js-cookie";
 
 const options = {
     host: process.env.MIX_ECHO_SERVER,
     port: process.env.MIX_ECHO_SERVER_PORT,
     transport: process.env.MIX_ECHO_SERVER_PROTOCOL,
+    headers: {
+        Authorization: "Bearer " + Cookies.get(process.env.MIX_ECHO_TOKEN),
+    },
 };
  
 export const $echo = new EchoClient(options);
